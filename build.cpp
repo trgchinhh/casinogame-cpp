a93055ff8867f0659f87d5a1dc0c3eb85d4d8917
Trường Chinh <156447398+trgchinhh@users.noreply.github.com>
2026-01-29 19:38:06 +0700

    Add files via upload

---
+++ build.cpp
@@ -0,0 +1,19 @@
+/*****************************************\
+ * Casino game code in C++
+ * Cấu trúc dữ liệu dựa trên DSLK kép 
+ * Gồm màu sắc và hiệu ứng âm thanh 
+\*****************************************/
+
+#include <iostream>
+using namespace std;
+
+const string tenchuongtrinh = "Casino.exe";
+const string lenhbiendich = "g++ src\\main.cpp src\\resource\\resource.o -o " + tenchuongtrinh + " -lwinmm -w";
+
+int main(){
+    system(lenhbiendich.c_str());
+    cout << "Đã biên dịch xong ! Chạy file (y/n): ";
+    char c; cin >> c;
+    if(c == 'y') system(tenchuongtrinh.c_str());
+    return 0;
+}
---
+++ src/lib/admin.h
@@ -0,0 +1,11 @@
+#include <string>
+using namespace std;
+
+// Dùng để phân quyền cho người thường và admin
+enum PhanQuyen {
+    Nguoichoi, Admin
+};
+
+// Tên tài khoản và mật khẩu admin
+const string TentaikhoanAdmin = "admin";
+const string MatkhauAdmin = "admin";
