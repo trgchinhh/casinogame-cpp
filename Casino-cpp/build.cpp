0e40bd3962e6636e42dd048d1cdcbf4c1af70de5
Trường Chinh <156447398+trgchinhh@users.noreply.github.com>
2026-02-02 08:58:15 +0700

    Update build.cpp

--- Casino-cpp/build.cpp
+++ Casino-cpp/build.cpp
@@ -13,11 +13,13 @@ const string lenhbiendich = string("g++ src\\main.cpp ")
                           + "-LC:\\OpenSSL-Win64\\lib src\\resource\\resource.o -o " 
                           + tenchuongtrinh 
                           + " -lwinmm -lssl -lcrypto -w";
-
+                          
 int main(){
     system(lenhbiendich.c_str());
     cout << "Đã biên dịch xong ! Chạy file (y/n): ";
     char c; cin >> c;
-    if(c == 'y') system(tenchuongtrinh.c_str());
+    if(c == 'y') {
+        system(tenchuongtrinh.c_str());
+    }
     return 0;
 }
