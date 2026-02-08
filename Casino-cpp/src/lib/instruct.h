e17c295c1f6895daa3774430402e5192bdcbae8b
Trường Chinh <156447398+trgchinhh@users.noreply.github.com>
2026-02-08 08:02:46 +0700

    Add files via upload

---
+++ Casino-cpp/src/lib/instruct.h
@@ -0,0 +1,67 @@
+#pragma once
+#include "..//lib//props.h"
+
+const string noidunghuongdan =
+"\n"
+
+RED ICON_USER "** TẠO TÀI KHOẢN **\n" RESET
+"\t• Tạo tên người dùng (Tên này có thể là nickname)\n"
+"\t• Tạo tên đăng nhập (ghi nhớ)\n"
+"\t• Tạo mật khẩu (ghi nhớ)\n"
+"\t• Tạo mã 6 số (buộc nhớ nếu muốn đổi mật khẩu)\n"
+"\t• Tạo số dư (tối đa 100.000.000)\n\n"
+
+RED ICON_DICE "** TÀI XỈU 1xx / 3xx **\n" RESET
+"\t• Nhập lựa chọn Tài hoặc Xỉu và nhập tiền cược\n"
+"\t• Chờ 5 giây sẽ có kết quả\n"
+"\t• Tài xỉu 1xx: 1 → 3 (Xỉu), 4 → 6 (Tài)\n"
+"\t• Tài xỉu 3xx: 3 → 10 (Xỉu), 11 → 18 (Tài)\n\n"
+
+RED ICON_NUMBER "** ĐOÁN DÀI NGẮN **\n" RESET
+"\t• Nhập lựa chọn Dài hoặc Ngắn và nhập tiền cược\n"
+"\t• Có hiệu ứng chạy số\n"
+"\t• Trên 30 là Dài, dưới hoặc bằng 30 là Ngắn\n\n"
+
+RED ICON_COLOR "** ĐOÁN 2 / 7 MÀU **\n" RESET
+"\t• Chọn màu bạn tin là sẽ ra (2 màu) và nhập tiền cược\n"
+"\t• Hệ thống random màu (2/7 màu)\n"
+"\t• Trúng 1 màu là ăn\n\n"
+
+RED ICON_NUMBER "** ĐOÁN SỐ A → B **\n" RESET
+"\t• Nhập khoảng số A đến B và nhập tiền cược\n"
+"\t• Đoán số trong khoảng đó\n"
+"\t• Trúng số random thì thắng\n\n"
+
+RED ICON_NUMBER "** CHẴN LẺ 0 → 9 **\n" RESET
+"\t• Nhập lựa chọn Chẵn hoặc Lẻ và nhập tiền cược\n"
+"\t• Hệ thống random số từ 0 đến 9\n"
+"\t• Trùng kết quả thì thắng\n\n"
+
+RED ICON_COIN "** ÚP NGỬA XU **\n" RESET
+"\t• Nhập kết quả Úp hoặc Ngửa và nhập tiền cược\n"
+"\t• Tung đồng xu ngẫu nhiên\n"
+"\t• Đúng thì thắng, sai thì thua\n\n"
+
+RED ICON_CARD "** XÌ DÁCH **\n" RESET
+"\t• Nhập tiền cược đầu tiên\n"
+"\t• Rút 2 lá sau đó chọn rút tiếp hoặc dừng\n"
+"\t• A = 1 điểm, J/Q/K = 10 điểm\n"
+"\t• Tổng điểm gần hoặc bằng 21 hơn bot thì thắng\n"
+"\t• Quá 21 là quắc\n\n"
+
+RED ICON_DICE "** LẮC BẦU CUA **\n" RESET
+"\t• Chọn linh vật muốn cược (Không giới hạn) \n"
+"\t• Nhập tiền cược\n"
+"\t• Lắc 3 xúc xắc\n"
+"\t• Trùng bao nhiêu mặt thì ăn bấy nhiêu lần\n\n"
+
+RED ICON_HAND "** KÉO BÚA BAO **\n" RESET
+"\t• Chọn Kéo / Búa / Bao và nhập tiền cược\n"
+"\t• So kết quả với bot\n"
+"\t• Thắng thì lụm tiền\n\n"
+
+RED ICON_WARN "** LƯU Ý **\n" RESET
+"\t[->] Thắng có thể x1.9 hoặc x2 tùy game\n"
+"\t[->] Nạp tiền vào acc Admin:\n\t\tUser: admin - Pass: admin\n"
+"\t[->] Có thể tạo tối đa 100 tài khoản\n"
+"\t[->] Nên bật hiệu ứng âm thanh để chơi vui hơn\n";
