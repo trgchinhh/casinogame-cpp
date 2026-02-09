59cef9c508f887e60b4791832f1ec46734e51247
Trường Chinh <156447398+trgchinhh@users.noreply.github.com>
2026-02-09 15:53:22 +0700

    Add files via upload

---
+++ Casino-cpp/src/game/bacao.h
@@ -0,0 +1,169 @@
+#include "..//lib//header.h"
+
+struct Labaibacao {
+    int giatribai;   // 1-13 (ách đến già) A -> K
+    int kyhieubai;   // 0-3  (4 ký hiệu cơ/rô/chuồng/bích)
+};
+
+vector<Labaibacao> taobobai52la_bacao() {
+    vector<Labaibacao> bobai;
+    for (int c = 0; c < 4; c++) {
+        for (int g = 1; g <= 13; g++) {
+            bobai.push_back({g, c}); 
+        }
+    }
+    return bobai;
+}
+
+void xaobaibacao(vector<Labaibacao>& bobai) {
+    random_device rd;
+    mt19937 gen(rd());
+    shuffle(bobai.begin(), bobai.end(), gen);
+}
+
+Labaibacao rutbaibacao(vector<Labaibacao>& bobai) {
+    Labaibacao b = bobai.back();
+    bobai.pop_back();
+    return b;
+}
+
+string tenbaibacao(const Labaibacao& b) {
+    string ten;
+    if (b.giatribai == 1) ten = "A";
+    else if (b.giatribai == 11) ten = "J";
+    else if (b.giatribai == 12) ten = "Q";
+    else if (b.giatribai == 13) ten = "K";
+    else ten = to_string(b.giatribai);
+    return ten;
+}
+
+bool kiemtrabacao(const vector<Labaibacao>& bai){
+    if(bai.size() != 3) return false;
+    for(auto& b : bai){
+        if(b.giatribai < 10) return false;
+    }
+    return true;
+}
+
+void game_bacao(DanhSachNguoiChoi& danhsachnguoichoi, ThongTinPtr& nguoichoi) {
+    if (!kiemtrasodutaikhoan(nguoichoi)) {
+        cout << YELLOW << "\t\t(!) Số dư tài khoản không đủ !" << RESET << endl;
+        return;
+    }
+    int tiencuoc = 0;
+    hieuungamthanh_mp3(dd_dichuyenmenu, trangthaiamthanh);
+    if (!nhaptiencuoc(tiencuoc, nguoichoi)){
+        return;
+    }
+    nguoichoi->sodu -= tiencuoc;
+    int phienhientai = phiencuoicung(nguoichoi->tentaikhoan) + 1;
+    system("cls");
+    inbanner(bannerbacao);
+    cout << "\n" << RED << "[GAME] " << RESET << "BA CÀO\n\n";
+    cout << "\tSố dư sau khi cược: " << YELLOW << dinhdangtien(nguoichoi->sodu) << RESET << " VND" << endl;
+    cout << YELLOW << "\tPhiên: " << RESET << phienhientai
+         << YELLOW << "\tTiền cược: " << RESET << dinhdangtien(tiencuoc) << " VND" << endl;
+    cout << endl;
+    // Khởi tạo bài và xào bài
+    vector<Labaibacao> bobai = taobobai52la_bacao();
+    xaobaibacao(bobai);
+    vector<Labaibacao> baibot, bainguoi;
+    // Phát mỗi người 3 lá
+    for (int i = 0; i < 3; i++) {
+        baibot.push_back(rutbaibacao(bobai));
+        bainguoi.push_back(rutbaibacao(bobai));
+    }
+    auto tinhdiembaicao = [](vector<Labaibacao>& bai) {
+        int tong = 0;
+        for (auto& b : bai) {
+            if (b.giatribai == 1) tong += 1;
+            else if (b.giatribai >= 10) tong += 0;
+            else tong += b.giatribai;
+        }
+        return tong % 10;
+    };
+    int diembot = tinhdiembaicao(baibot);
+    int diemnguoi = tinhdiembaicao(bainguoi);
+    
+    // In bài bot 
+    cout << "\t[" << BLUE << "Bài bot" << RESET << "]: ";
+    for (auto& b : baibot)
+        cout << "[" << RED << tenbaibacao(b) << RESET << "] ";
+    if(diembot == 0){
+        if(kiemtrabacao(baibot)) 
+            cout << " -->" << YELLOW << " Ba cào" << RESET << endl; 
+    }
+    else cout << " --> " << YELLOW << diembot << RESET << " nút" << endl;
+    cout << endl;
+
+    // Mở bài người chơi
+    cout << "\t>> Nhấn phím bất kỳ để nặn bài !" << endl << endl;
+    cout << "\t[" << GREEN << "Bài bạn" << RESET << "]: ";
+    cout << "[-] [-] [-] (Đang che)" << endl;
+    //cout << "\t    │" << endl;
+
+    _getch();
+    for (int i = 0; i < 3; i++) {
+        //if(i < 2) cout << "\t├─ [" << GREEN << "Bài bạn" << RESET << "]: ";
+        //else cout << "\t└─ [" << GREEN << "Bài bạn" << RESET << "]: ";
+        if(i < 1) cout << RED << "\t   -" << RESET << " Mở lá " << i + 1 << ": ";
+        else if(i == 1) cout << RED << "\t   -" << RESET << " Mở lá " << i + 1 << ": ";
+        else cout << RED << "\t   -" << RESET << " Mở lá " << i + 1 << ": "; 
+
+        int diemtungla = 0;
+        for (int j = 0; j <= i; j++){
+            cout << "[" << RED << tenbaibacao(bainguoi[j]) << RESET << "] ";
+            // in tổng nút những lá đã mở 
+            // if (bainguoi[j].giatribai == 1) diemtungla += 1;
+            // else if (bainguoi[j].giatribai >= 10) diemtungla += 0;
+            // else diemtungla += bainguoi[j].giatribai;
+        
+            // in số nút của lá hiện tại (lá đang mở)
+            if (bainguoi[i].giatribai == 1) diemtungla = 1;
+            else if (bainguoi[i].giatribai >= 10) diemtungla = 0;
+            else diemtungla = bainguoi[i].giatribai;
+        }
+        for (int j = i + 1; j < 3; j++)
+            cout << "[-] ";
+        cout << " --> " << YELLOW << diemtungla << RESET << " nút";
+        cout << endl;
+        if(i < 2) _getch();
+    }
+    cout << endl;
+    if(diemnguoi == 0){
+        if(kiemtrabacao(bainguoi)) cout << "\tTổng nút của bạn: " << YELLOW << "3 Cào" << RESET << endl;
+        else cout << "\tTổng nút của bạn: " << YELLOW << "Bù" << RESET << endl;
+    }
+    else cout << "\tTổng nút của bạn: " << YELLOW << diemnguoi << RESET << " nút" << endl;
+    cout << endl;
+
+    string ketqua;
+    if (diemnguoi > diembot) ketqua = "Thắng";
+    else if (diemnguoi < diembot) ketqua = "Thua";
+    else ketqua = "Hòa";
+    cout << YELLOW << "\tKết quả: " << RESET << "[" << ketqua << "]" << endl;
+    if (ketqua == "Hòa") {
+        nguoichoi->sodu += tiencuoc;
+        cout << "\tTrả lại tiền: " << YELLOW << "~" << dinhdangtien(tiencuoc) << RESET << " VND" << endl;
+        hieuungamthanh_mp3(dd_dunglacontraicuata, trangthaiamthanh);
+    } else if (ketqua == "Thắng") {
+        int tienthang = tiencuoc * 2;
+        nguoichoi->sodu += tienthang;
+        cout << GREEN << "\tChúc mừng bạn đã thắng bot" << RESET 
+             << "\n\tTiền thắng: " << YELLOW << "+" << dinhdangtien(tienthang) << RESET << " VND" << endl;
+        hieuungamthanh_mp3(dd_dunglacontraicuata, trangthaiamthanh);
+    } else {
+        cout << RED << "\tChúc bạn may mắn lần sau" << RESET
+             << "\n\tTiền thua: " << YELLOW << "-" << dinhdangtien(tiencuoc) << RESET << " VND" << endl;
+    }
+    LichSu lichsu;
+    lichsu.phien = phienhientai;
+    lichsu.trochoi = "BaCao";
+    lichsu.sodu = nguoichoi->sodu;
+    lichsu.tiencuoc = tiencuoc;
+    lichsu.luachon = "Lat bai tung la";
+    lichsu.tongdiem = diemnguoi;
+    lichsu.ketqua = ketqua;
+    luulichsujson(nguoichoi->tentaikhoan, lichsu);
+    luudulieujson(danhsachnguoichoi);
+}
