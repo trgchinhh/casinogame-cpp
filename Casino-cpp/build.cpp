d579dffb001a2094d9e32ec517f53edcdcab0001
Trường Chinh <156447398+trgchinhh@users.noreply.github.com>
2026-02-19 22:37:40 +0700

    Update build.cpp

--- Casino-cpp/build.cpp
+++ Casino-cpp/build.cpp
@@ -8,7 +8,7 @@
 // Bản quyền: MIT LICENSE 2026
 
 /*******************************************************\
- * Hoàn thành ngày 10/02/2026                          *
+ * Hoàn thành ngày 19/02/2026                          *
  * Cấu trúc dữ liệu dựa trên danh sách liên kết kép    *
  * Gồm màu sắc chữ và hiệu ứng âm thanh                *
  * Lưu data và lịch sử ở các file định dạng JSON       *
@@ -45,4 +45,5 @@ int main(){
     }
     return 0;
 }
+
 
