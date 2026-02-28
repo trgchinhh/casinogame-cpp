7641ffa98f4628722567a2b6fec5feeb8d98545f
Trường Chinh <chinhcuber@gmail.com>
2026-02-28 20:37:53 +0700

    Create info.h

---
+++ src/lib/info.h
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+#define RESET   "\033[0m"
+#define RED     "\033[38;2;255;120;120m"
+#define GREEN   "\033[38;2;120;255;120m"
+#define YELLOW  "\033[38;2;255;255;150m"
+
+const string noidungthongtin =
+"\n"
+"        ┌───────────────────────────────────────────────────┐\n"
+"        │          " RED "CASINO GAMES CONSOLE CUSTOM TUI" RESET "          │\n"
+"        ├───────────────────────────────────────────────────┤\n"
+"        │ (" RED "◉" RESET ") " YELLOW "Phiên bản:" RESET " v1.3.0                             │\n"
+"        │ (" RED "◉" RESET ") " YELLOW "Hoàn thành:" RESET " 28/02/2026                        │\n"
+"        │ (" RED "◉" RESET ") " YELLOW "Giấy phép:" RESET " MIT                                │\n"
+"        │ (" RED "◉" RESET ") " YELLOW "Bản quyền:" RESET " © 2026 - Nguyễn Trường Chinh       │\n"
+"        │ (" RED "◉" RESET ") " YELLOW "Github:" RESET " github.com/trgchinhh/casinogame-cpp   │\n"
+"        └───────────────────────────────────────────────────┘\n";
