edc4fdd0f3ed0285a38311dc0750edf4cdf8f509
Trường Chinh <chinhcuber@gmail.com>
2026-03-10 21:38:55 +0700

    Update info.h

--- src/lib/info.h
+++ src/lib/info.h
@@ -10,6 +10,7 @@ using namespace std;
 #define RED     "\033[38;2;255;120;120m"
 #define GREEN   "\033[38;2;120;255;120m"
 #define YELLOW  "\033[38;2;255;255;150m"
+#define CYAN         "\033[38;2;0;255;255m"
 #define UNDERLINE    "\033[4m"
 #define NO_UNDERLINE "\033[24m"
 
@@ -18,9 +19,9 @@ const string noidungthongtin =
 "\t┌───────────────────────────────────────────────────┐\n"
 "\t│          " RED "CASINO GAMES CONSOLE CUSTOM TUI" RESET "          │\n"
 "\t├───────────────────────────────────────────────────┤\n"
-"\t│ (" RED "◉" RESET ") " YELLOW "Phiên bản:" RESET " v1.3.0                             │\n"
-"\t│ (" RED "◉" RESET ") " YELLOW "Hoàn thành:" RESET " 28/02/2026                        │\n"
-"\t│ (" RED "◉" RESET ") " YELLOW "Giấy phép:" RESET " MIT                                │\n"
-"\t│ (" RED "◉" RESET ") " YELLOW "Bản quyền:" RESET " © 2026 - Nguyễn Trường Chinh       │\n"
-"\t│ (" RED "◉" RESET ") " YELLOW "Github: " RESET UNDERLINE "github.com/trgchinhh/casinogame-cpp" NO_UNDERLINE "   │\n"
+"\t│ (" RED "◉" RESET ") " CYAN "Phiên bản:" RESET " v1.3.0                             │\n"
+"\t│ (" RED "◉" RESET ") " CYAN "Hoàn thành:" RESET " 28/02/2026                        │\n"
+"\t│ (" RED "◉" RESET ") " CYAN "Giấy phép:" RESET " MIT                                │\n"
+"\t│ (" RED "◉" RESET ") " CYAN "Bản quyền:" RESET " © 2026 - Nguyễn Trường Chinh       │\n"
+"\t│ (" RED "◉" RESET ") " CYAN "Github: " RESET UNDERLINE "github.com/trgchinhh/casinogame-cpp" NO_UNDERLINE "   │\n"
 "\t└───────────────────────────────────────────────────┘\n";
