f59588b6d2185348880134aac003290b4856a2ae
Trường Chinh <chinhcuber@gmail.com>
2026-05-02 17:47:31 +0700

    Update build.cpp

--- build.cpp
+++ build.cpp
@@ -16,12 +16,28 @@
  * Có thể viết thêm game trong folder src/game/        *
 \*******************************************************/
 
+#ifdef _WIN32               // Nếu là Windows 
+    #define NOMINMAX
+    #include <windows.h>
+    #include <conio.h>
+    #include <mmsystem.h>       // phát hiệu ứng âm thanh
+#else                       // Nếu là Linux hoặc HDH khác
+    #include <sys/ioctl.h>
+    #include <unistd.h> 
+    #include <termios.h>
+#endif
+
 #include <iostream>
 #include <exception>
 #include <filesystem>
+#include <thread>
+#include <atomic>
+#include <chrono>
 
 using namespace std;
 namespace fs = filesystem;
+using namespace chrono;
+using namespace this_thread;
 
 #define RESET   "\033[0m"
 #define RED     "\033[38;2;255;120;120m"
@@ -54,6 +70,33 @@ bool cacfilecanbiendich() {
     return false;
 }
 
+void ancontrochuot(bool trangthaicontrochuot){
+    #ifdef _WIN32
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        CONSOLE_CURSOR_INFO cursorInfo;
+        GetConsoleCursorInfo(hConsole, &cursorInfo);
+        cursorInfo.bVisible = trangthaicontrochuot;
+        SetConsoleCursorInfo(hConsole, &cursorInfo);
+    #else
+        if (trangthaicontrochuot) cout << "\033[?25h"; 
+        else cout << "\033[?25l";
+    #endif
+}
+
+void hieuung_cho(atomic<bool>& dung){
+    ancontrochuot(false);
+    const char* spinner[] = {"⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"};
+    int i = 0;
+    while(!dung){
+        cout << "\r" << spinner[i] 
+             << RESET << " Vui lòng chờ biên dịch !" << flush;
+        i = (i + 1) % 10;
+        sleep_for(milliseconds(100));
+    }
+    cout << "\r                                             \r";
+    ancontrochuot(true);
+}
+
 int main(){
     try {
         // Kiểm tra tồn tại bin (tạo thư mục bin)
@@ -63,13 +106,18 @@ int main(){
             system("mkdir -p bin");
         #endif
         if(cacfilecanbiendich()){
-            cout << "Đang biên dịch ! Vui lòng chờ..." << endl;
+            //cout << "Đang biên dịch ! Vui lòng chờ..." << endl;
             //cout << "Source thay đổi -> Đang biên dịch lại..." << RESET << endl;
+            atomic<bool> stop(false);
+            thread loading(hieuung_cho, ref(stop));
             int ketqua = system(lenhbiendich.c_str());
+            stop = true;
+            loading.join();
             if(ketqua != 0){
                 throw runtime_error(RED "Biên dịch thất bại !");
                 exit(0);
             }
+            cout << "Biên dịch thành công !" << endl;
         } 
         else cout << "Không có thay đổi -> dùng file build cũ" << RESET << endl;
         cout << "Chạy chương trình (y/n): ";
